4e85a7bb983d89c4d3b6530d5ea4b0ac53530abf
LizaVydritskaia <liza.vydritkaia@gmail.com>
2023-04-28 19:28:09 +0300

    Fixed project files

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { HomesGuestsLoves } from '../HomesGuestsLoves';
+import { Sprite } from '../Sprite';
+import { MainContent } from '../MainContent';
+import { TopSection } from '../TopSection';
+import { AvailableHotelsClass } from '../AvailableHotelsClass';
+import { HomesGuestsLovesClass } from '../HomesGuestsLovesClass';
 
 export const App = () => {
+  const [searchParams, setSearchParams] = useState('');
+  const [showAvailableHotelsBlock, setShowAvailableHotelsBlock] =
+    useState(false);
+
   return (
     <>
-      <HomesGuestsLoves />
+      <Sprite />
+      <MainContent>
+        <TopSection
+          setSearchParams={setSearchParams}
+          setShowAvailableHotelsBlock={setShowAvailableHotelsBlock}
+        />
+        <AvailableHotelsClass
+          searchParams={searchParams}
+          showAvailableHotelsBlock={showAvailableHotelsBlock}
+        />
+        <HomesGuestsLovesClass />
+      </MainContent>
     </>
   );
 };
--- src/components/AvailableHotelsClass/AvailableHotelsClass.jsx
+++ src/components/AvailableHotelsClass/AvailableHotelsClass.jsx
@@ -7,20 +7,44 @@ import { Title } from '../Title';
 
 import './AvailableHotelsClass.css';
 
+import { getAvailableHotels } from '../../services/availableHotels';
+
 export class AvailableHotelsClass extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      availableHotels: [],
+    };
+
+    this.filterHotels = this.filterHotels.bind(this);
+  }
+
+  componentDidMount() {
+    getAvailableHotels(this.props.searchParams).then((hotels) =>
+      this.setState({ availableHotels: hotels }),
+    );
+  }
+
+  filterHotels() {
+    return this.state.availableHotels.filter((item) => {
+      return (
+        item.name.toLowerCase().includes(this.props.searchParams) ||
+        item.city.toLowerCase().includes(this.props.searchParams) ||
+        item.country.toLowerCase().includes(this.props.searchParams)
+      );
+    });
   }
 
   render() {
     return (
       this.props.showAvailableHotelsBlock && (
         <section className="available-hotels">
+          {this.props.searchParams}
           <Container>
             <Title content="Available hotels" />
             <div className="homes__hotels">
               <Hotel
-                hotelsDataArray={this.props.filteredArray}
+                hotelsDataArray={this.filterHotels}
                 className="col-lg-3 col-md-6 col-sm-3"
               />
               <Arrow />
